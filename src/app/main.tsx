fb7214c256f13b2c02e0ad091360a62fa316957b
lanovich <kolaemelanov60@gmail.com>
2026-01-15 00:26:55 +0300

    feat: add pwa

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -8,3 +8,15 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </AppProvider>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('SW registered:', registration);
+      })
+      .catch(error => {
+        console.error('SW registration failed:', error);
+      });
+  });
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      filename: "sw.js",
+      strategies: "injectManifest",
+      injectRegister: false,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
